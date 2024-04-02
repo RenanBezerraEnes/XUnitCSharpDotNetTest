bce1275e6715c7c45f2b6eecd98d71e3f266321c
Renan Bezerra <renanbezerraenes@icloud.com>
2024-04-02 10:12:25 +0200

    Initial xUnit tests.

---
+++ XUnitCSharpDotNetTest.UnitTest/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using XUnitCSharpDotNetTest.Ping;
+
+namespace XUnitCSharpDotNetTest.UnitTest.PingTests
+{
+    public class NetworkServiceTests
+    {
+        private readonly NetworkService _pingService;
+
+        public NetworkServiceTests()
+        {
+            _pingService = new NetworkService();
+        }
+
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+
+            var result = _pingService.SendPing();
+
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("SUCCESS: Ping Sent");
+            result.Should().Contain("SUCCESS", Exactly.Once());
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        public void NetworkService_PingTimeOut_ReturnInt(int a, int b, int expected)
+        {
+            var pingService = new NetworkService();
+
+            var result = pingService.PingTimeOut(a, b);
+
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-10000, 0);
+        }
+    }
+};
---
+++ XUnitCSharpDotNetTest/Ping/NetworkService.cs
@@ -0,0 +1,15 @@
+﻿namespace XUnitCSharpDotNetTest.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            return "SUCCESS: Ping Sent";
+        }
+
+        public int PingTimeOut(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
